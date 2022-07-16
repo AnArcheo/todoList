a595b868b762c0268cd941271f9c705940c8a37b
Anna Łuczak <an.archeo@gmail.com>
2022-07-16 21:14:35 +0100

    User entity

---
+++ src/main/java/com/springboot/todolist/model/User.java
@@ -0,0 +1,23 @@
+package com.springboot.todolist.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
+
+}
