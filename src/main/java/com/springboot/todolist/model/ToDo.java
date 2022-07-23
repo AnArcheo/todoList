6ec0ec6a5e98768636e2c4c0adc69a3b6e7d9a31
Anna Łuczak <an.archeo@gmail.com>
2022-07-23 21:52:13 +0100

    updated todo

--- src/main/java/com/springboot/todolist/model/ToDo.java
+++ src/main/java/com/springboot/todolist/model/ToDo.java
@@ -19,23 +19,27 @@ public class ToDo {
     private Long id;
 
     private String description;
-    private Status status;
-    private Priority priority;
-    private boolean isCompleted;
-    private Instant createdDate;
-    private Instant modifiedDate;
-    private Instant endDate;
 
-    private String timeLeft;
+    @Enumerated(EnumType.STRING)
+    @Column(length=9)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length=12)
+    private Priority priority;
+    @Column
+    private boolean isCompleted;
+    @Column
+    private Instant createdDate;
+    @Column
+    private Instant modifiedDate;
 
     @Autowired
     public ToDo(String description, Status status, Priority priority,
-                boolean isCompleted, Instant createdDate, Instant modifiedDate,
-                Instant endDate, String timeLeft){
+                boolean isCompleted){
         super();
         createdDate = Instant.now();
         modifiedDate = Instant.now();
     }
-
 
 }
