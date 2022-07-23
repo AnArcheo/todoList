117fd6f1d30b2acb47df7ccc4f6781787fa77465
Anna Łuczak <an.archeo@gmail.com>
2022-07-23 21:52:56 +0100

    updated todo repository

--- src/main/java/com/springboot/todolist/repositories/ToDoRepository.java
+++ src/main/java/com/springboot/todolist/repositories/ToDoRepository.java
@@ -1,7 +1,8 @@
 package com.springboot.todolist.repositories;
 
 import com.springboot.todolist.model.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ToDoRepository extends CrudRepository<ToDo, Long> {
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
 }
