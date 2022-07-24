6d1ee904fa4bd9fc595e0536aeae5df6c0b97fcb
Anna Łuczak <an.archeo@gmail.com>
2022-07-24 23:36:49 +0100

    updated TodoController

--- src/main/java/com/springboot/todolist/controllers/TodoController.java
+++ src/main/java/com/springboot/todolist/controllers/TodoController.java
@@ -1,15 +1,42 @@
 package com.springboot.todolist.controllers;
 
+import com.springboot.todolist.model.ToDo;
+import com.springboot.todolist.repositories.ToDoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 public class TodoController {
 
-    @GetMapping("/")
-    public ModelAndView todoView(){
-        ModelAndView model = new ModelAndView("todoView");
-        return model;
+
+    private ToDoRepository toDoRepository;
+
+    public TodoController(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
     }
+
+
+//    public Set<ToDo> findAllTodos(){
+//        Set<ToDo> todoSet = new HashSet<>();
+//        toDoRepository.findAll().iterator().forEachRemaining(todoSet::add);
+//        return todoSet;
+//    }
+
+    @RequestMapping({"", "/"})
+    public String getTodoViewPage(Model model) {
+        model.addAttribute("todos", toDoRepository.findAll());
+        return "todoView";
+    }
+//    @GetMapping("/")
+//    public String todoView(){
+//        ModelAndView model = new ModelAndView("todoView");
+//        return "todoView";
+//    }
 }
